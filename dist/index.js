98ec14b2e9261b5e17154b1d74f8dda0ff1589d6
Niall Donnelly <niall.donnelly@feedhenry.com>
2017-02-23 17:26:52 +0000

    Moved Worker logic from Portal Application

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,5 @@
+require('./empty.tpl.html.js');
+require('./workorder-display.tpl.html.js');
 require('./workorder-form.tpl.html.js');
 require('./workorder-list.tpl.html.js');
 require('./workorder.tpl.html.js');
