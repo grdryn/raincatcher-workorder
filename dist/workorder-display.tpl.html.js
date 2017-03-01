77ab09fe2c6f459d309f4bb79c4726a0b6e52fcd
Niall Donnelly <niall.donnelly@feedhenry.com>
2017-03-01 17:10:12 +0000

    Removed Angular Functionality

--- dist/workorder-display.tpl.html.js
+++ dist/workorder-display.tpl.html.js
@@ -14,10 +14,10 @@ ngModule.run(['$templateCache', function ($templateCache) {
     '    </h3>\n' +
     '\n' +
     '    <span flex></span>\n' +
-    '    <md-button class="md-icon-button" aria-label="Edit" ui-sref="app.workorder.edit({workorderId: ctrl.workorder.id})">\n' +
+    '    <md-button ng-if="ctrl.adminMode" class="md-icon-button" aria-label="Edit" ui-sref="app.workorder.edit({workorderId: ctrl.workorder.id})">\n' +
     '      <md-icon md-font-set="material-icons">edit</md-icon>\n' +
     '    </md-button>\n' +
-    '    <md-button class="md-icon-button" aria-label="Delete" ng-click="ctrl.delete($event, ctrl.workorder)">\n' +
+    '    <md-button ng-if="ctrl.adminMode" class="md-icon-button" aria-label="Delete" ng-click="ctrl.delete($event, ctrl.workorder)">\n' +
     '      <md-icon md-font-set="material-icons">delete</md-icon>\n' +
     '    </md-button>\n' +
     '  </div>\n' +
@@ -27,7 +27,7 @@ ngModule.run(['$templateCache', function ($templateCache) {
     '\n' +
     '  <workorder workorder="ctrl.workorder" status="ctrl.result.status" assignee="ctrl.assignee"></workorder>\n' +
     '\n' +
-    '  <md-card>\n' +
+    '  <md-card ng-if="ctrl.adminMode">\n' +
     '    <md-progress-linear md-mode="determinate" ng-value="ctrl.progress"></md-progress-linear>\n' +
     '    <md-card-title>\n' +
     '      <md-card-title-text>\n' +
@@ -38,14 +38,14 @@ ngModule.run(['$templateCache', function ($templateCache) {
     '      <p class="md-body-1" ng-if="! ctrl.result.stepResults || ctrl.result.stepResults | isEmpty">\n' +
     '        No workflow submissions.\n' +
     '      </p>\n' +
-    '      <workflow-result result="ctrl.result" workflow="ctrl.workflow"></workflow-result>\n' +
+    '      <workflow-result ng-if="ctrl.adminMode" result="ctrl.result" workflow="ctrl.workflow"></workflow-result>\n' +
     '    </md-card-content>\n' +
     '  </md-card>\n' +
     '\n' +
     '</div><!-- wfm-maincol-scroll -->\n' +
     '\n' +
     '\n' +
-    '<md-button class="md-fab" aria-label="New Workorder" ui-sref="app.workorder.new">\n' +
+    '<md-button ng-if="ctrl.adminMode" class="md-fab" aria-label="New Workorder" ui-sref="app.workorder.new">\n' +
     '  <md-icon md-font-set="material-icons">add</md-icon>\n' +
     '</md-button>\n' +
     '');
